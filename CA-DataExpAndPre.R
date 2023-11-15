613bfc8ad1d62d622cd058a152b693912cef28b8
2020300 <2020300.student.cct.ie>
2023-11-15 19:47:47 +0000

    Read data

---
+++ CA-DataExpAndPre.R
@@ -0,0 +1,12 @@
+# CA - 
+# Lecturer's Name: 
+# Student Name:
+#
+
+# Read csv
+data <- read.csv("C:/Users/henri/Documents/CA-DataExpAndPre/RecordedCrimeOffences.csv")
+
+# This dataset contains information about Recorded Crime Offences, which Gard Station it was recorded
+# And which crime was comitted.
+# Display data
+head (data)
