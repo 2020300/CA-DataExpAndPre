c03f9a1c7cc0db3da0a3b1358032a827ca4cdfee
2020300 <2020300.student.cct.ie>
2023-11-27 16:34:11 +0000

    Few more tasks included

--- CA-DataExpAndPre.R
+++ CA-DataExpAndPre.R
@@ -3,16 +3,12 @@
 # Student Name:
 #
 
-
-install.packages("devtools")
-devtools::install_github("ropensci/skimr")
-
+#Import libraries needed
 library(lubridate)
 library(ggplot2)
 library(skimr)
 library(tidyr)
 skimr::skim(data)
-
 
 # Read csv
 data <- read.csv("C:/Users/henri/Documents/CA-DataExpAndPre/CovidVacRate.csv")
@@ -25,8 +21,13 @@ head(data)
 # Check structure
 str(data)
 
-# Transform age into numeric value
+## DATA PREPARING ##
+
+# Transform age and other columns with wrong formation into numeric value
 data$Age.Group <- as.numeric(data$Age.Group)
+data$VALUE <- as.numeric(data$VALUE)
+data$Year <- as.numeric(data$Year)
+
 # Replace '5 - 11 years' with random ages between 5 and 11
 data$Age.Group <- ifelse(data$`Age.Group` == '5 - 11 years',
                  sample(5:11, sum(data$`Age.Group` == '5 - 11 years'), replace = TRUE),
@@ -43,9 +44,15 @@ head(data$Age.Group)
 # Separate the Month column into Month and Year
 data <- separate(data, Month, into = c("Year", "Month"), sep = " ")
 
+# Assign "Fully Vaccinated" as 1 and not Fully Vaccinated 2, to make it easier to manipulate the data
+data$Vaccination_Code <- ifelse(data$Statistic.Label == "Fully Vaccinated", 1, 2)
+
+# Removing updated data
+data <- subset(data, select = -C03898V04649)
+
 # Display the modified dataset
+str(data)
 print(data)
 
 # Now we age, month and year fixed we can start manipulating our data
-
 hist(data$Age.Group, xlab = "Age", main = "Age Distribution")
